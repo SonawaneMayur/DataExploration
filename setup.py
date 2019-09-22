644ffaa2472a54f38373f28b3a7006b8e32f2aef
SonawaneMayur <sonawane.m@husky.neu.edu>
2019-09-22 16:41:39 -0500

    updated filename and setuptools.filename

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="data-analysis",
+    name="Exploratory_Data-Analysis",
     version="0.0.1",
     author="Mayur Sonawane",
     author_email="mayurs1690@gmail.com",
