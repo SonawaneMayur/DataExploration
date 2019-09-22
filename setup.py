f134ad80af4700b6ac81a81a4bffd3b96d920a43
SonawaneMayur <sonawane.m@husky.neu.edu>
2019-09-22 16:36:35 -0500

    updated github link

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="analyse the parquet file and generate the descriptive stats, character encoding, regression analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/",
+    url="https://github.com/SonawaneMayur/Exploratory_Data_Analysis.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
