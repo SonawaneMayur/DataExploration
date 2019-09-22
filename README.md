dd18462272b8218a8e249951291e18823a543fd7
SonawaneMayur <sonawane.m@husky.neu.edu>
2019-09-22 17:35:06 -0500

    first time commit of DataExploration package

---
+++ README.md
@@ -0,0 +1,3 @@
+Analysis of all Big Data files like parquet file and
+generate the descriptive stats, character encoding,
+regression analysis for each column exist in the file
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Exploratory_Data_Analysis",
+    name="DataExploration",
     version="0.0.1",
     author="Mayur Sonawane",
     author_email="sonawane.m@husky.neu.edu",
     description="analyse the parquet file and generate the descriptive stats, character encoding, regression analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/SonawaneMayur/Exploratory_Data_Analysis.git",
+    url="https://github.com/SonawaneMayur/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
